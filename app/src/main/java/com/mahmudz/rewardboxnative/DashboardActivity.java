2f3494b415be7a6dbd399204c156274ed824d5dd
mahmudz <mahmudbappy.pri@gmail.com>
2020-01-22 20:12:47 +0600

    Initial commit

---
+++ app/src/main/java/com/mahmudz/rewardboxnative/DashboardActivity.java
@@ -0,0 +1,20 @@
+package com.mahmudz.rewardboxnative;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class DashboardActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_dashboard);
+
+    }
+}
